a1fc6110113d0fd6ebb04cfdc7d87226d55b57ad
Dovydas Savickas dagas199@gmail.com <dagas199@gmail.com>
2021-04-07 19:15:13 +0200

    share code on github

---
+++ frontend/src/App.js
@@ -0,0 +1,62 @@
+import './App.css';
+import data from './data';
+
+function App() {
+  return (
+    <div className="grid-container">
+            <header className="row">
+                <div>
+                    <a  className="brand" href="/">Tina shop</a>
+                </div>
+                <div>
+                    <a href="/cart">Cart</a>
+                    <a href="/signin">Sign in</a>
+                </div>
+            </header>
+            <main>
+                <div className="row center">
+                  {
+                    data.products.map((product) => (
+                    <div key={product._id} className="card">
+                        <a href={`/product/${product._id}`}>
+                            <img className="medium" src={product.image} alt={product.name}></img>
+                        </a>
+                        <div className="card-body">
+                            <a href="product.html">
+                                <a href={`/product/${product._id}`}></a>
+                            </a>
+                            <div className="rating">
+                                <span>
+                                    <i className="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i className="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i className="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i className="fa fa-star"></i>
+                                </span>
+                                <span>
+                                    <i className="fa fa-star"></i>
+                                </span>
+                                <div className={product.price}>
+                                    $120
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    ))
+                  };
+                    
+                </div>
+            </main>
+            <footer className="row center">
+                All right reserved
+            </footer>
+        </div>
+  );
+}
+
+export default App;
