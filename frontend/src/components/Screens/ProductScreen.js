d0dfb8337274106e0b47bb6fe271f7966fd5b821
Dovydas Savickas <dagas199@gmail.com>
2021-04-09 22:32:20 +0200

    added product page to redux

--- frontend/src/components/Screens/ProductScreen.js
+++ frontend/src/components/Screens/ProductScreen.js
@@ -1,62 +1,78 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import data from '../../data';
+import { detailsProduct } from '../../actions/productActions';
+import LoadingBox from '../LoadingBox';
+import MessageBox from '../MessageBox';
 import Rating from '../Rating';
 
 export default function ProductScreen(props) {
-    const product = data.products.find((x)=> x._id === props.match.params.id)
-    if(!product){
-        return <div>Product not found</div>
-    }
+    const dispatch = useDispatch();
+    const productId = props.match.params.id;
+    const productDetails = useSelector(state => state.productDetails);
+    const { loading, error, product } = productDetails;
+
+    useEffect(() =>{
+        dispatch(detailsProduct(productId));
+    }, [dispatch, productId]);
+
     return (
         <div>
-            <Link to="/">Back to result</Link>
-           <div className="row top">
-               <div className="col-2">
-                    <img class="large" src={product.image} alt={product.image}/>
-               </div>
-               <div className="col-1">
-                <ul>
-                    <li>
-                        <h1>{product.name}</h1>
-                    </li>
-                    <li>
-                        <Rating rating={product.rating}></Rating>
-                    </li>
-                    <li>Price : ${product.price}</li>
-                    <li>Description:
-                        <p>{product.description}</p>
-                    </li>
-                </ul>
-               </div>
-               <div className="col-1">  
-                    <div class="card card-body">
-                        <ul>
-                            <li>
-                                <div className="row">
-                                    <div>Price</div>
-                                    <div className="price">${product.price}</div>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="row">
-                                    <div>Status: </div>
-                                    <div>
-                                        {product.countInStock>0? (
-                                            <span className="success">In Stock</span>
-                                        ) : (
-                                            <span className="danger">Unavailable</span>
-                                        )}
+            {loading? (<LoadingBox />
+            ) : error ? (
+            <MessageBox variant="dam">{error}</MessageBox>
+            ) : (
+            <div>
+                <Link to="/">Back to result</Link>
+            <div className="row top">
+                <div className="col-2">
+                        <img class="large" src={product.image} alt={product.image}/>
+                </div>
+                <div className="col-1">
+                    <ul>
+                        <li>
+                            <h1>{product.name}</h1>
+                        </li>
+                        <li>
+                            <Rating rating={product.rating}></Rating>
+                        </li>
+                        <li>Price : ${product.price}</li>
+                        <li>Description:
+                            <p>{product.description}</p>
+                        </li>
+                    </ul>
+                </div>
+                <div className="col-1">  
+                        <div class="card card-body">
+                            <ul>
+                                <li>
+                                    <div className="row">
+                                        <div>Price</div>
+                                        <div className="price">${product.price}</div>
                                     </div>
-                                </div>
-                            </li>
-                            <li>
-                                <button className="primary block">Add To Cart</button>
-                            </li>
-                        </ul>
-                    </div>
-               </div>
-           </div>
+                                </li>
+                                <li>
+                                    <div className="row">
+                                        <div>Status: </div>
+                                        <div>
+                                            {product.countInStock>0? (
+                                                <span className="success">In Stock</span>
+                                            ) : (
+                                                <span className="danger">Unavailable</span>
+                                            )}
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <button className="primary block">Add To Cart</button>
+                                </li>
+                            </ul>
+                        </div>
+                </div>
+            </div>
+            </div>
+            
+            )}
         </div>
     )
 }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,12 @@
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import ReduxThunk from 'redux-thunk';
 import data from "./data";
-import { productListReducer } from './reducers/productReducers';
+import { productDetailsReducer, productListReducer } from './reducers/productReducers';
 
 const initialState = {};
 const reducer = combineReducers({
     productList: productListReducer,
+    productDetails: productDetailsReducer,
 })
 const composeEchancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
