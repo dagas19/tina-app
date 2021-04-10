82f0a4bad1020f03426afce8575df0350dcb13bd
Dovydas Savickas <dagas199@gmail.com>
2021-04-10 10:45:38 +0200

    implemented add to cart action

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,38 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 import './App.css';
 import CartScreen from './components/Screens/CartScreen';
 import HomeScreen from './components/Screens/HomeScreen';
 import ProductScreen from './components/Screens/ProductScreen';
 
 function App() {
+	const cart = useSelector((state) => state.cart);
+	const { cartItems } = cart;
 	return (
-		<div className="grid-container">
-			<header className="row">
-				<div>
-					<a className="brand" href="/">
-						Tina shop
-					</a>
-				</div>
-				<div>
-					<a href="/cart">Cart</a>
-					<a href="/signin">Sign in</a>
-				</div>
-			</header>
-			<main>
-				<BrowserRouter>
+		<BrowserRouter>
+			<div className="grid-container">
+				<header className="row">
+					<div>
+						<Link className="brand" to="/">
+							Tina shop
+						</Link>
+					</div>
+					<div>
+						<Link to="/cart">
+							Cart
+							{cartItems.length > 0 && <span className="badge">{cartItems.length}</span>}
+						</Link>
+						<Link to="/signin">Sign in</Link>
+					</div>
+				</header>
+				<main>
 					<Route path="/cart/:id?" component={CartScreen}></Route>
 					<Route path="/product/:id" component={ProductScreen}></Route>
 					<Route path="/" component={HomeScreen} exact></Route>
-				</BrowserRouter>
-			</main>
-			<footer className="row center">All right reserved</footer>
-		</div>
+				</main>
+				<footer className="row center">All right reserved</footer>
+			</div>
+		</BrowserRouter>
 	);
 }
 
