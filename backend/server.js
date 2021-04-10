35f717d38f941fee7ab91ed5f20625bfb1f06879
Dovydas Savickas <dagas199@gmail.com>
2021-04-10 21:08:10 +0200

    added mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,35 @@
-import express from 'express';
-import data from './data.js';
+import express from "express";
+import mongoose from "mongoose";
+import data from "./data.js";
+import userRouter from "./routers/userRouter.js";
 
 const app = express();
-
-app.get('/api/products/:id', (req, res) => {
-	const product = data.products.find((x) => x._id === req.params.id);
-	if (product) {
-		res.send(product);
-	} else {
-		res.status(404).send({ message: 'Product not found' });
-	}
-});
-app.get('/api/products', (req, res) => {
-	res.send(data.products);
+mongoose.connect("mongodb://localhost/tina-app", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
-app.get('/', (req, res) => {
-	res.send('Server is ready');
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product not found" });
+  }
 });
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
+});
+app.use("/api/users", userRouter);
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
 app.listen(5000, () => {
-	console.log('Serve at http://localhost:5000');
+  console.log("Serve at http://localhost:5000");
 });
