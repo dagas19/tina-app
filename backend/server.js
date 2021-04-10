9e52af5764c7529f539e3d199a81cb9371657a9a
Dovydas Savickas <dagas199@gmail.com>
2021-04-10 23:47:55 +0200

    added signin backend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
 
+dotenv.config();
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 mongoose.connect("mongodb://localhost/tina-app", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
