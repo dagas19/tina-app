b5b18968180dc4eec1c23a0c6c329f35820c1dba
Dovydas Savickas <dagas199@gmail.com>
2021-04-17 19:33:29 +0200

    added port

--- backend/server.js
+++ backend/server.js
@@ -40,6 +40,8 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-app.listen(5000, () => {
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
   console.log("Serve at http://localhost:5000");
 });
