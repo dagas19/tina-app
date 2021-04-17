dab8531f8f3278e594a3f4a8baa6334165c3279a
Dovydas Savickas <dagas199@gmail.com>
2021-04-17 22:09:41 +0200

    fixed h

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/tina-app", {
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/tina-app", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
