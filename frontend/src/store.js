237beb39c20f2fdabe43b83f1504ed2e9cd3858f
Dovydas Savickas <dagas199@gmail.com>
2021-04-17 18:10:00 +0200

    added delete order button

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,6 +3,7 @@ import ReduxThunk from "redux-thunk";
 import { cartReducer } from "./reducers/cartReducers";
 import {
   orderCreateReducer,
+  orderDeleteReducer,
   orderDetailsReducer,
   orderListReducer,
   orderMineListReducer,
@@ -55,6 +56,7 @@ const reducer = combineReducers({
   productUpdate: productUpdateReducer,
   productDelete: productDeleteReducer,
   orderList: orderListReducer,
+  orderDelete: orderDeleteReducer,
 });
 const composeEchancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
